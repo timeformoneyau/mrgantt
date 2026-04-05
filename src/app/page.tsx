55663e0a09560b22372b97eab5b5d1e489212e14
timeformoneyau <timeformoneyau@gmail.com>
2026-04-06 08:28:01 +0930

    Sync Gantt chart data from server on mount
    Add useEffect to synchronize Gantt chart data from server on component mount.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 
+import { useEffect } from 'react'
 import dynamic from 'next/dynamic'
+import { useGanttStore } from '@/store/ganttStore'
 
 const GanttChart = dynamic(() => import('@/components/gantt/GanttChart').then(m => m.GanttChart), {
   ssr: false,
 })
 
 export default function Home() {
+  const syncFromServer = useGanttStore((s) => s.syncFromServer)
+
+  useEffect(() => {
+    syncFromServer()
+  }, [syncFromServer])
+
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
       <GanttChart />
